3f42ce6d7b8b85de61179a29e1e6f747114ad71a
NahianShabab <53574953+NahianShabab@users.noreply.github.com>
2022-05-24 16:45:53 +0600

    Factory Pattern Example

---
+++ Design Patterns/Creational Design Patterns/Factory/Example 0 Shape Factory/RectangularShape/RectangularShape.java
@@ -0,0 +1,10 @@
+package RectangularShape;
+
+import Shape.Shape;
+
+public class RectangularShape implements Shape {
+    @Override
+    public void drawShape(){
+        System.out.println("Drawing Rectangle");
+    }
+}
---
+++ Design Patterns/Creational Design Patterns/Factory/Example 0 Shape Factory/TriangularShape/TriangularShape.java
@@ -0,0 +1,10 @@
+package TriangularShape;
+
+import Shape.Shape;
+
+public class TriangularShape implements Shape {
+    @Override
+    public void drawShape(){
+        System.out.println("Drawing Triangle");
+    }
+}
